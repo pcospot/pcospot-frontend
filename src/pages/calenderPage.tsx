4cf9fbdef8305af9695a4a5eff2e6ca2af2cf536
officeshinyujun <officeshinyujun@gmail.com>
2024-11-15 21:50:17 +0900

    real final

--- src/pages/calenderPage.tsx
+++ src/pages/calenderPage.tsx
@@ -4,17 +4,21 @@ import CalenderSideBar from "../components/calender/calenderSideBar.tsx";
 import useCalendarStore from "../components/Stores/useCalendarStore.tsx";
 import returnLogin from "../function/returnLogin.tsx";
 import Header from "../components/header/header.tsx";
+import {useState} from "react";
 
 export default function CalenderPage(){
+    const [openSidebar, setOpenSidebar] = useState(true);
     const calendarStore = useCalendarStore(state => state.calendarEvent);
     returnLogin()
     const schedule = calendarStore
 
     return(
         <>
-            <Header/>
+            <Header onToggleSidebar={() => {setOpenSidebar(!openSidebar)}}/>
             <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-                <CalenderSideBar schedule={schedule}/>
+                {openSidebar && (
+                    <CalenderSideBar schedule={schedule}/>
+                )}
                 <Calender events={schedule}></Calender>
                 <RightSideServerBar />
             </div>
